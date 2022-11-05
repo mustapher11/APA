1522499ee9241fd06efbabfffc390e2e917e59f3
mustapher11 <mustapher1997@gmail.com>
2022-11-04 17:22:38 -0700

    Initial commit of APA Project.

--- src/main/java/com/mia/apa/PrintReceiptController.java
+++ src/main/java/com/mia/apa/PrintReceiptController.java
@@ -8,6 +8,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 import java.net.URL;
 import java.util.Objects;
@@ -50,6 +53,9 @@ public class PrintReceiptController implements Initializable {
         PrinterJob printerJob = PrinterJob.createPrinterJob();
         JobSettings jobSettings = printerJob.getJobSettings();
         PageLayout pageLayout = jobSettings.getPageLayout();
+
+        Font font = Font.font("Freeroad Bold.ttf", FontWeight.EXTRA_BOLD, FontPosture.ITALIC, 8);
+        textArea.setFont(font);
 
         if (!generatedReceipt.isEmpty() || !generatedReceipt.isBlank()) {
             if (printers != null && !printers.isEmpty()) {
